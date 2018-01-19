1d31d4980ac8ca9db2ee1d7b2d922d834ac4c086
Kirill Vercetti <office@kyzima-spb.com>
2018-01-19 22:42:44 +0300

    Add README and edit setup.py

---
+++ README.rst
@@ -0,0 +1,82 @@
+Pony Database Facade
+====================
+
+|PyPI| |LICENCE| |STARS|
+
+PonyORM for your Flask application.
+
+
+Installation
+------------
+
+::
+
+    pip install flask-pony
+
+
+Quick start
+-----------
+
+Read the documentation for the `pony-database-facade`_ package.
+
+.. code:: python
+
+    # app.py
+
+    from flask import Flask
+
+    from flask_pony import Pony
+
+
+    app = Flask(__name__)
+    app.config.from_object('configmodule.Config')
+
+    pony = Pony(app)
+
+    from . import models
+
+    pony.connect()
+
+
+.. code:: python
+
+    # models.py
+
+    from pony.orm import Required
+
+    from . import pony
+
+
+    db = pony.db
+
+
+    class Person(db.Entity):
+        username = Required(str, 50)
+
+
+.. code:: python
+
+    # configmodule.py
+
+    class Config(object):
+        PONY = {
+            'provider': 'mysql',
+            'user': 'anyone',
+            'password': 'anykey',
+            'dbname': 'blog'
+        }
+
+
+.. |PyPI| image:: https://img.shields.io/pypi/v/flask-pony.svg
+   :target: https://pypi.python.org/pypi/flask-pony/
+   :alt: Latest Version
+
+.. |LICENCE| image:: https://img.shields.io/github/license/kyzima-spb/flask-pony.svg
+   :target: https://github.com/kyzima-spb/flask-pony/blob/master/LICENSE
+   :alt: Apache 2.0
+
+.. |STARS| image:: https://img.shields.io/github/stars/kyzima-spb/flask-pony.svg
+   :target: https://github.com/kyzima-spb/flask-pony/stargazers
+
+.. _Русская документация: docs/RU.rst
+.. _pony-database-facade: https://github.com/kyzima-spb/pony-database-facade
--- setup.py
+++ setup.py
@@ -1,27 +1,36 @@
-"""
-Flask-Pony
-----------
+# coding: utf-8
+#
+# Copyright 2017 Kirill Vercetti
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-PonyORM for your Flask application.
-
-GitHub
-------
-
-`Uses and documentation <https://github.com/kyzima-spb/flask-pony>`_
-
-"""
+import os
 from setuptools import setup, find_packages
 
 
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+
 setup(
-    name='Flask-Pony',
-    version='2.0.0',
+    name='flask-pony',
+    version='3.0.0',
     url='https://github.com/kyzima-spb/flask-pony',
     license='Apache-2.0',
     author='Kirill Vercetti',
     author_email='office@kyzima-spb.com',
     description='PonyORM for your Flask application',
-    long_description=__doc__,
+    long_description=README,
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
@@ -31,7 +40,7 @@ setup(
         'pony-database-facade'
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        "Development Status :: 4 - Beta",
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
