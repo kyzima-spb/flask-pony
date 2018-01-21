de108538db0fac332b975bf39bf0d18316e68e91
Kirill Vercetti <office@kyzima-spb.com>
2018-01-21 16:43:44 +0300

    New simple example

--- examples/example-2.py
+++ examples/example-2.py
@@ -1,1 +1,23 @@
 # coding: utf-8
+
+from flask import Flask
+from pony.orm import Required
+
+from flask_pony import Pony
+
+
+app = Flask(__name__)
+
+pony = Pony()
+pony.init_app(app)
+
+db = pony.db
+
+
+class User(db.Entity):
+    name = Required(str, 50)
+
+
+pony.connect()
+
+app.run(debug=True)
