c5c591d3e528828e5a759aad1cd89c06b43cf088
Kirill Vercetti <office@kyzima-spb.com>
2018-05-30 14:31:32 +0300

    db_session now starts automatically

--- flask_pony/__init__.py
+++ flask_pony/__init__.py
@@ -14,12 +14,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import print_function
+from __future__ import print_function, unicode_literals
 
-from flask import current_app
+from flask import current_app, has_app_context, has_request_context
+from pony.orm import db_session
+from pony.orm.core import local
 from pony_database_facade import DatabaseFacade
 
 __version__ = '3.0.1'
+
+
+def has_db_session():
+    """Returns True if db_session exists"""
+    return local.db_context_counter > 0
+
+
+def start_db_session():
+    """Starts a new db_session if it does not exists"""
+    # print('==> Start session')
+
+    if has_db_session():
+        return
+
+    if not has_app_context() or not has_request_context():
+        raise RuntimeError('You need app_context or request_context')
+
+    db_session.__enter__()
+
+
+def stop_db_session(exc=None):
+    """Stops the last db_session"""
+    # print('==> Stop session', type(exc))
+
+    if has_db_session():
+        exc_type = None
+        tb = None
+
+        if exc:
+            exc_type, tb = type(exc), exc.__traceback__
+
+        db_session.__exit__(exc_type, exc, tb)
 
 
 class Pony(object):
@@ -56,3 +90,10 @@ class Pony(object):
     def init_app(self, app):
         self.app = app
         app.config.setdefault('PONY', {})
+
+        app.before_request(start_db_session)
+
+        @app.teardown_appcontext
+        def shutdown_session(response_or_exc):
+            stop_db_session(response_or_exc)
+            return response_or_exc
